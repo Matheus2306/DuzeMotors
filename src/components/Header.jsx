fcc0d201ac3877441d1d60648556a6f197258618
matheus2306 <math2306.r@gmail.com>
2025-05-20 16:14:04 -0300

    add Header component and global styles; integrate Bootstrap for styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <nav
+        className="navbar bg-black navbar-expand-lg p-2 justify-content-around"
+        onClick={handleLogoClick}
+        role="button"
+      >
+        <div className="d-flex align-items-center gap-2">
+          <i className="bi bi-bicycle text-danger fs-2"></i>
+          <span className="text-light fw-bold fs-5">DuzéMotors</span>
+        </div>
+        <div className="d-flex align-items-center text-light gap-5 fw-bold">
+          <span>Inicio</span>
+          <span>Catalogo</span>
+          <span>Sobre</span>
+          <span>Contato</span>
+        </div>
+
+        <div>
+          <i className="bi bi-cart text-light fs-5"></i>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
