b867ad4196165389ed42e4b6bd9cee819c452e57
Bryan <bryanratin8@gmail.com>
2025-05-20 17:01:10 -0300

    correção do banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Banner = () => {
+  return (
+    <section className="d-flex align-items-center justify-content-center text-center text-white"  style={{
+        backgroundColor: '#000',
+        height: '100%',
+        maxHeight: '700px',
+        padding: '60px 20px' 
+      }}>
+      <div>
+        <h1 className="fw-bold display-4">
+          Sua Nova <span className="text-danger">Moto</span> Está Aqui
+        </h1>
+        <p className="mt-3 fs-5">
+          Descubra nossa coleção exclusiva de motos de alta performance e design inovador.<br />
+          Qualidade, confiança e o melhor preço do mercado.
+        </p>
+        <div className="d-flex justify-content-center gap-3 mt-4">
+          <button className="btn btn-danger btn-lg">
+            Ver Catálogo →
+          </button>
+          <button className="btn btn-light btn-lg text-dark">
+            Agendar Test Ride
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
