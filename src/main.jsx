50be6bb67b3a41b47ffeccf3f4a1b574463bc55c
Bryan <bryanratin8@gmail.com>
2025-06-04 08:00:17 -0300

    correção do merge

--- src/main.jsx
+++ src/main.jsx
@@ -17,24 +17,6 @@ import Cartao from "./Pages/Cartao.jsx";
 import PagamentoConcluido from "./Pages/PagamentoConcluido.jsx";
 import Gerenciamento from "./Pages/Gerenciamento.jsx";
 import UserManagement from "./Pages/UserManagement.jsx";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Catalogo from './pages/Catalogo';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import Chekout from './Pages/Chekout.jsx'
-import Cadastro from './Pages/Cadastro.jsx'
-import Login from './Pages/Login.jsx'
-import NotFound from './Pages/NotFound.jsx'
-import Sobre from './Pages/Sobre.jsx'
-import Perfil from './components/Login/Perfil.jsx'
-import DetalhesMoto from './Pages/DetalhesMoto.jsx';
-import Cartao from './Pages/Cartao.jsx'
-import PagamentoConcluido from './Pages/PagamentoConcluido.jsx';
-import Gerenciamento from './Pages/Gerenciamento.jsx';
 import ProdFornecedores from './Pages/ProdFornecedores.jsx';
 
 createRoot(document.getElementById("root")).render(
